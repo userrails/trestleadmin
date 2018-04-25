6c8b1da8468c962e90308be4190b2c61c13eaf89
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-04-25 14:00:31 +0545

    working with books_admin resource

---
+++ app/admin/books_admin.rb
@@ -0,0 +1,59 @@
+Trestle.resource(:books) do
+  menu do
+    item :books, icon: "fa fa-circle"
+  end
+
+  # by default all rails column will display e.g: is, name, created, updated
+  table do
+    column :name
+    column :created_at, align: :center
+    column :updated_at
+
+    actions
+  end
+
+  # Customize the table columns shown on the index view.
+  #
+  # table do
+  #   column :name
+  #   column :created_at, align: :center
+  #   actions
+  # end
+
+  # Customize the form fields shown on the new/edit views.
+  #
+  # form do |book|
+  #   text_field :name
+  #
+  #   row do
+  #     col(xs: 6) { datetime_field :updated_at }
+  #     col(xs: 6) { datetime_field :created_at }
+  #   end
+  # end
+
+  form do |book|
+    text_field :name
+
+    # you can hide this code as it is auto saved
+    row do
+      col(xs: 6) { datetime_field :updated_at }
+      col(xs: 6) { datetime_field :created_at }
+    end
+  end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:book).permit(:name, ...)
+  # end
+
+  params do |params|
+    # you can filter required parameters
+    params.require(:book).permit(:name, :created_at, :updated_at)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425075328) do
+ActiveRecord::Schema.define(version: 20180425080442) do
 
   create_table "administrators", force: :cascade do |t|
     t.string "email"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20180425075328) do
     t.string "name"
     t.text "description"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
